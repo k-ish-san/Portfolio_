3dc121006b6ba59eb707a383f7135361e7e393bd
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-06-22 19:57:50 +0530

    fix: remove unused theme configuration from tailwind.config.ts and format vite.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,14 +7,5 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", // core source files
   ],
  
-
-  theme: {
-    extend: {
-      fontFamily: {
-        orbitron: ["Orbitron", "sans-serif"],
-      },
-    },
-  },
-
   plugins: [], // ✅ Add Tailwind plugins here if needed later
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import glsl from "vite-plugin-glsl";
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+    modulePreload: true,
+    cssCodeSplit: true,
+    minify: "esbuild",
+    sourcemap: false,
+  },
   plugins: [react(), glsl()],
-})
+});
