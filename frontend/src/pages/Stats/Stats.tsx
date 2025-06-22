c1551864096495d84d12af4f8428588eb5a77657
samrudhkishsanpm <samrudhkishsanpm@gmail.com>
2025-06-22 20:47:42 +0530

    fix: update loading fallback in Stats component to use Loader component

--- frontend/src/pages/Stats/Stats.tsx
+++ frontend/src/pages/Stats/Stats.tsx
@@ -1,13 +1,13 @@
 import { lazy, Suspense } from "react";
 import { Card } from "../../components/Card";
-
+import Loader from "../../components/Loader";
 // Lazy load the GitHubCalendar component
 const Dashboard = lazy(() => import("./Dashboard"));
 
 const Stats = () => {
   return (
     <Card title="Stats">
-      <Suspense fallback={<div className="h-10">Loading stats...</div>}>
+      <Suspense fallback={<div className="h-10"><Loader load={true} /></div>}>
         <Dashboard />
       </Suspense>
     </Card>
